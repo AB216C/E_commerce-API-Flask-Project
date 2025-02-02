33e541d70ae536d2c949d918be39a676f52d9c98
Ferdinand Niyongire <ferdinandniyongira@gmail.com>
2025-02-02 05:23:58 -0600

    New Project

---
+++ app.py
@@ -0,0 +1,212 @@
+from flask import Flask,request,jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from marshmallow import ValidationError
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey,Table, String, Column,DateTime, func, select, Date
+from typing import List
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Mahirane231995@localhost/E_COMMERCE'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+ma = Marshmallow(app)
+
+
+class User(Base):
+  __tablename__ = "users"
+  user_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+  name: Mapped[str] = mapped_column(String(100))
+  address: Mapped[str] = mapped_column(String(100))
+  email: Mapped[str] = mapped_column(String(100), unique= True)
+
+  orders:Mapped[List["Order"]] = relationship(back_populates="buyer")
+
+order_product = Table(
+  "order_product",
+  Base.metadata,
+  Column("product_id", ForeignKey("products.product_id")),
+  Column("order_id", ForeignKey("orders.order_id"))
+)
+
+class Order(Base):
+  __tablename__ = "orders"
+  order_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+  order_date = mapped_column(DateTime, default=func.now())
+  user_id: Mapped[int] = mapped_column(ForeignKey('users.user_id'))
+  
+  buyer: Mapped["User"] = relationship(back_populates="orders")
+
+  products: Mapped[List["Product"]] =relationship( secondary="order_product", back_populates="purchase_orders")
+
+
+class Product(Base):
+  __tablename__ = "products"
+  product_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+  product_name: Mapped[str] = mapped_column(String(100))
+  price: Mapped[float] = mapped_column(nullable=False)
+
+  purchase_orders: Mapped[List["Order"]] = relationship(secondary="order_product", back_populates = "products")
+
+
+class UserSchema(ma.SQLAlchemyAutoSchema):
+  class Meta:
+    model = User
+
+class OrderSchema(ma.SQLAlchemyAutoSchema):
+  class Meta:
+    model = Order
+class ProductSchema(ma.SQLAlchemyAutoSchema):
+  class Meta:
+    model = Product
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+order_schema = OrderSchema()
+orders_schema = OrderSchema(many=True)
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
+
+#==============USER END POINT =====================================
+@app.route("/user", methods=['POST'])
+
+#This function can be used to add multiple users to the API
+def create_user():
+  try:
+    new_user = user_schema.load(request.json)
+  except ValidationError as e:
+    return jsonify(e.messages),400
+
+  user = User(name=new_user['name'], address=new_user['address'], email=new_user['email'])
+  db.session.add(user)
+  db.session.commit()
+
+  return user_schema.dumps(new_user),201
+
+#The route/function below has ability to retrieve all users added to the API in JSON format.
+@app.route("/users", methods = ['GET'])
+
+def get_users():
+  query = select(User)
+  users = db.session.execute(query).scalars().all()
+  return users_schema.dump(users),200
+
+#This route and function has ability to retrive a specific using id.
+@app.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    user = db.session.get(User, id)
+    return user_schema.jsonify(user), 200
+
+#This route and function has ability to update name, address, or email of a specific user based on the assigned id
+@app.route('/users/<int:id>', methods=['PUT'])
+
+def update_user(id):
+  user = db.session.get(User,id)
+
+  if not user:
+    return jsonify({"message":"Invalid user id"}),400
+  
+  try:
+    user_data = user_schema.load(request.json)
+  except ValidationError as e:
+    return jsonify(e.messages),400
+  
+  user.name =user_data['name']
+  user.address = user_data['address']
+  user.email = user_data['email']
+  
+  db.session.commit()
+  return user_schema.jsonify(user),200
+
+@app.route('/users/<int:id>', methods=['DELETE'])
+
+def delete_user(id):
+    user = db.session.get(User,id)
+
+    if not user:
+      return jsonify({"message":"Invalid user id"}),400
+    
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"Messagge":f"Deleted user: {id}"}),200
+
+#=============== PRODUCT END POINTS =========================
+# #This function can be used to add multiple product to the API
+
+@app.route("/product", methods=['POST'])
+def create_product():
+  try:
+    new_product = product_schema.load(request.json)
+  except ValidationError as e:
+    return jsonify(e.messages),400
+
+  product = Product(product_name=new_product['product_name'], price=new_product['price'])
+  db.session.add(product)
+  db.session.commit()
+
+  return product_schema.dumps(new_product),201
+
+
+# #The route/function below has ability to retrieve all product added to the API in JSON format.
+@app.route("/products", methods = ['GET'])
+
+def get_products():
+  query = select(Product)
+  products = db.session.execute(query).scalars().all()
+  return products_schema.dump(products),200
+
+
+# #This route and function has ability to retrive a specific using id.
+@app.route('/products/<int:id>', methods=['GET'])
+def get_product(id):
+    product = db.session.get(Product, id)
+    return product_schema.jsonify(product), 200
+
+
+# #This route and function has ability to update name, address, or email of a specific product based on the assigned id
+@app.route('/product/<int:id>', methods=['PUT'])
+
+def update_product(id):
+  product= db.session.get(Product,id)
+
+  if not product:
+    return jsonify({"message":"Invalid product id"}),400
+  
+  try:
+    product_data = product_schema.load(request.json)
+  except ValidationError as e:
+    return jsonify(e.messages),400
+  
+  product.product_name =product_data['product_name']
+  product.price = product_data['price']
+
+  db.session.commit()
+  return product_schema.jsonify(product),200
+
+
+#Deleting products based on product id
+@app.route('/products/<int:id>', methods=['DELETE'])
+
+def delete_product(id):
+    product = db.session.get(Product,id)
+
+    if not product:
+      return jsonify({"message":"Invalid product id"}),400
+    
+    db.session.delete(product)
+    db.session.commit()
+    return jsonify({"Messagge":f"Deleted product: {id}"}),200
+
+
+if __name__ == "__main__":
+  with app.app_context():
+    db.create_all()
+  app.run(debug=True)
+
+
